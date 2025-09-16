7efd538568a0beca0be6d821b1bde489fe19c2d6
segalancar1 <segalancar@gmail.com>
2025-09-16 10:10:19 +0700

    tambah file

---
+++ Resep.java
@@ -0,0 +1,9 @@
+public class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Kraby Paty");
+        System.out.println("===================");
+        System.out.println("1. Kecoa");
+        System.out.println("2. Nasi Kering");
+        System.out.println("Jepitan Baju");
+    }
+}
